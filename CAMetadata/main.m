33fc72664494bd09a187a566ab08ef8cc3fde11a
brownfeng <brownfeng@webank.com>
2016-07-21 16:16:51 +0800

    Initial Commit

---
+++ CAMetadata/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CAMetadata
+//
+//  Created by brownfeng on 16/7/21.
+//  Copyright © 2016年 brownfeng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
